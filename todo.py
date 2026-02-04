861f56c62f15ed0bfc84fb02fd4834a878cfc246
Toby Panday <tobypanday@gmail.com>
2026-02-04 15:08:46 -0400

    added time tracker

--- todo.py
+++ todo.py
@@ -1,14 +1,19 @@
 # To-do list thing
 
+from datetime import datetime
+
+
 def enter_tasks():
     tasks = input("Enter your tasks: ").split(", ")
     return tasks
 
 
 def store_tasks(tasks):
+    current_date = datetime.now()
     with open("tasks.txt", "a") as file:
         for task in tasks:
             file.write(task + "\n")
+        file.write(str(current_date))
 
 
 # run program
