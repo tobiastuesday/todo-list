743d0045590cd72843e0a32c3bc6b18270eefcb9
Toby Panday <tobypanday@gmail.com>
2026-02-03 22:58:03 -0400

    started a todo list project :)

---
+++ todo_list.py
@@ -0,0 +1,18 @@
+# To-do list thingy
+# unfinished
+
+def enter_tasks():
+    tasks = input("Enter your tasks: ").split(", ")
+    return tasks
+
+
+def store_tasks(tasks):
+    with open("tasks.txt", "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+
+
+# run program
+tasks = enter_tasks()
+store_tasks(tasks)
+print("Your tasks have been saved to tasks.txt")
