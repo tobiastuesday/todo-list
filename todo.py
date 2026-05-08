9a9c820a7dc5df1c6fe9553b834dfd06f03facf7
Toby Panday <tobypanday@gmail.com>
2026-05-08 14:55:54 -0400

    re-implemented data storage with json file instead of txt file

--- todo.py
+++ todo.py
@@ -1,22 +1,35 @@
 # To-do list thing
 
+import json
 from datetime import datetime
 
 
-def enter_tasks():
-    tasks = input("Enter your tasks: ").split(", ")
-    return tasks
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
 
-def store_tasks(tasks):
-    current_date = datetime.now()
-    with open("tasks.txt", "a") as file:
-        file.write(str(current_date) + "\n")
-        for task in tasks:
-            file.write(task + "\n")
+def add_new_tasks(task_list):
+    new_tasks = input("Enter your tasks (separate with commas): ").split(", ")
+    for task in new_tasks:
+        new_task_item = {
+            "Task": task,
+            "Timestamp": datetime.now().isoformat(),
+            "Status": "Pending"
+        }
+        task_list.append(new_task_item)
+
+
+def store_tasks(all_tasks):
+    with open("tasks.json", "w") as file:
+        json.dump(all_tasks, file, indent=4)
 
 
 # run program
-tasks = enter_tasks()
-store_tasks(tasks)
-print("Your tasks have been saved to tasks.txt")
+my_tasks = load_tasks()
+add_new_tasks(task_list=my_tasks)
+store_tasks(all_tasks=my_tasks)
+print("Your tasks have been saved to tasks.json")
